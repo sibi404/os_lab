6eb2fa70a8299920b475ef78b294f324600f9c46
sibi <sibikottukandiyil2@gmail.com>
2023-05-30 13:31:15 +0530

    IPC programs added

---
+++ IPC/shared_receiver.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/shm.h>
+#include<string.h>
+int main(){
+	void *shared_memory;
+	int shmid;
+	shmid = shmget((key_t)1122,1024,0666);
+	printf("Key of shared memory is : %d \n",shmid);
+	shared_memory = shmat(shmid,NULL,0);
+	printf("Process attached at %p\n",shared_memory);
+	printf("Data read from shared memory is : %s\n",(char *)shared_memory);
+	return 0;
+}
---
+++ IPC/shared_sender.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/shm.h>
+#include<string.h>
+int main(){
+	void *shared_memory;
+	char buff[20];
+	int shmid;
+	shmid = shmget((key_t)1122,1024,0666|IPC_CREAT);
+	printf("Key for shared memory is %d : \n",shmid);
+	shared_memory = shmat(shmid,NULL,0);
+	printf("Process attached at %p\n",shared_memory);
+	printf("Enter some data to write to shared memory : \n");
+	read(0,buff,20);
+	strcpy(shared_memory,buff);
+	printf("You wrote : %s\n",(char *)shared_memory);
+}
